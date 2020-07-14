806f0cd505d2b4fbe637627973321545240d1f7c
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-07-14 09:09:18 +0200

    Add some plugin stuff

--- src/main/java/me/alexprogrammerde/BungeeData/Main.java
+++ src/main/java/me/alexprogrammerde/BungeeData/Main.java
@@ -10,11 +10,20 @@ public class Main extends Plugin {
         plugin = this;
         Logger logger = this.getLogger();
 
-        logger.info("§aRegistering command");
+        logger.info("§6Registering command");
         getProxy().getPluginManager().registerCommand(this, new MainCommand("bungeedata", "bungeedata.use"));
 
-        logger.info("§aLoading metrics");
+        logger.info("§6Loading metrics");
         Metrics metrics = new Metrics(this, 8182);
+
+        /*logger.info("§6Checking for update");
+        new UpdateChecker(this, 81430).getVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                logger.info("§6There is not a new update available.");
+            } else {
+                logger.info("§cThere is a new update available. Its: " + version);
+            }
+        });*/
     }
 
     @Override
